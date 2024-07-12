004d2d89b2fb73a5f4ed59cb4f5c09c8579c6fa9
Esteban Tejeda <etejedaw@hotmail.com>
2024-07-12 19:37:35 -0400

    chore: add default values in environment module

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -2,12 +2,13 @@ import { z } from "zod";
 
 const EnvironmentSchema = z
 	.object({
-		PORT: z.string().transform(Number),
-		DB_NAME: z.string(),
-		DB_USERNAME: z.string(),
-		DB_PASSWORD: z.string(),
-		DB_HOST: z.string(),
-		DB_PORT: z.string().transform(Number)
+		PORT: z.string().default("3000").transform(Number),
+		ENV: z.string().default("dev"),
+		DB_NAME: z.string().default("madiadb"),
+		DB_USERNAME: z.string().default("root"),
+		DB_PASSWORD: z.string().default("toor"),
+		DB_HOST: z.string().default("127.0.0.1"),
+		DB_PORT: z.string().default("3306").transform(Number)
 	})
 	.readonly();
 
