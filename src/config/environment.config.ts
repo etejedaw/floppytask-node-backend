8d8f3b359c739caa3189475420816d480a5f87eb
Esteban Tejeda <etejedaw@hotmail.com>
2024-07-15 16:23:17 -0400

    fix(config): rename DB_NAME in environment variables

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -4,7 +4,7 @@ const EnvironmentSchema = z
 	.object({
 		PORT: z.string().default("3000").transform(Number),
 		NODE_ENV: z.string().default("dev"),
-		DB_NAME: z.string().default("madiadb"),
+		DB_NAME: z.string().default("mariadb"),
 		DB_USERNAME: z.string().default("root"),
 		DB_PASSWORD: z.string().default("toor"),
 		DB_HOST: z.string().default("127.0.0.1"),
