1952509f314ae223264da2289bd1a8e2fcd1ac24
Esteban Tejeda <etejedaw@hotmail.com>
2024-08-03 05:32:24 -0400

    chore(users): add password policy en register shcema

--- src/users/schemas/register.schema.ts
+++ src/users/schemas/register.schema.ts
@@ -1,10 +1,25 @@
 import { z } from "zod";
 
+const PasswordPolicy = z
+	.string()
+	.min(8, { message: "Password must be at least 8 characters long" })
+	.max(15, { message: "Password must be at most 15 characters long" })
+	.regex(/[a-z]/, {
+		message: "Password must contain at least one lowercase letter"
+	})
+	.regex(/[A-Z]/, {
+		message: "Password must contain at least one uppercase letter"
+	})
+	.regex(/\d.*\d/, { message: "Password must contain at least two numbers" })
+	.regex(/[^a-zA-Z0-9]/, {
+		message: "Password must contain at least one special character"
+	});
+
 export const RegisterSchema = z
 	.object({
 		name: z.string().optional(),
 		email: z.string().email(),
-		password: z.string()
+		password: PasswordPolicy
 	})
 	.strict();
 
