4a7dccb97333f72d4fdda0bdcf16633db79f5206
Esteban Tejeda <etejedaw@hotmail.com>
2024-08-14 04:08:00 -0400

    chore(config): change default DB_NAME variable

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -5,7 +5,7 @@ const EnvironmentSchema = z
 	.object({
 		PORT: z.string().default("3000").transform(Number),
 		NODE_ENV: z.nativeEnum(NodeEnv).default(NodeEnv.DEVELOPMENT),
-		DB_NAME: z.string().default("mariadb"),
+		DB_NAME: z.string().default("sys"),
 		DB_USERNAME: z.string().default("root"),
 		DB_PASSWORD: z.string().default("toor"),
 		DB_HOST: z.string().default("127.0.0.1"),
